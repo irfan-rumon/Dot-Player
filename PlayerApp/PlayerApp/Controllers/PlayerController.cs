35e22fa4d67a7ff8674291c04b5599f0c5d5a069
irfan-rumon <irfannisho8571@gmail.com>
2022-12-12 15:25:23 +0600

    Add form for creating player

---
+++ PlayerApp/PlayerApp/Controllers/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PlayerApp.Data;
+using PlayerApp.Models;
+
+namespace PlayerApp.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public PlayerController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Player player)
+        {
+            if(ModelState.IsValid)
+            {
+                await _db.AddAsync(player);
+                await _db.SaveChangesAsync();
+
+            }
+            return RedirectToAction(nameof(Index)); ;
+        }
+    }
+}
